02330264efa20bb54b8b431bcd747bde0ba78e28
Satvik Saha <sahasatvik@gmail.com>
2021-04-05 20:54:23 +0530

    Commented code

--- welearn_bot.py
+++ welearn_bot.py
@@ -2,11 +2,11 @@
 
 from requests import Session
 from bs4 import BeautifulSoup as bs
-import re
-import shutil, os, sys
+import os, sys
 import argparse
 import urllib
 
+# Get command line options
 parser = argparse.ArgumentParser(description="WeLearn bot.")
 
 parser.add_argument("courses", nargs="*", help="IDs of the courses to download files from")
@@ -17,8 +17,7 @@ if len(args.courses) == 0 and not args.listcourses:
     print("No course names selected. Use the -h flag for usage.")
     sys.exit()
 
-# Create a file in your home directory called .welearnrc
-# Put your username and password on separate lines
+# Read the .welearnrc file from the home directory, and extract username and password
 configfile = os.path.expanduser("~/.welearnrc")
 username = ''
 password = ''
@@ -28,6 +27,7 @@ with open(configfile, "r") as config:
     password = lines[1].strip()
 
 with Session() as s:
+    # Login to WeLearn with supplied credentials
     login_page = s.get("https://welearn.iiserkol.ac.in/login/")
     login_content = bs(login_page.content, "html.parser")
     token = login_content.find("input", {"name": "logintoken"})["value"]
@@ -35,7 +35,8 @@ with Session() as s:
     s.post("https://welearn.iiserkol.ac.in/login/", login_data)
     home_page = s.get("https://welearn.iiserkol.ac.in/my/")
     home_content = bs(home_page.content, "html.parser")
-        
+    
+    # Build a list of courses and course page links
     courses_list = home_content.find("li", {"data-key": "mycourses"})
     course_links = courses_list.findAll("a", "list-group-item")
     courses = dict()
@@ -44,24 +45,30 @@ with Session() as s:
         course_name = course.find("span").contents[0]
         courses[course_name] = course_url
     
+    # If specified, only display the list of courses and exit
     if args.listcourses:
         for name in courses.keys():
             print(name)
         sys.exit()
     
+    # Loop through all given course IDs
     for selected_course_name in args.courses:
+        # Ensure that the course name is valid
         if not selected_course_name in courses.keys():
             print(selected_course_name + " not in list of available courses!")
             continue
+
+        # Get the course page and extract all links
         selected_course_page = s.get(courses[selected_course_name])
         selected_course_content = bs(selected_course_page.content, "html.parser")
-        
         links = selected_course_content.findAll('a', "aalink")
         
         for link in links:
+            # Only download 'resource' links
             if ('/mod/resource/view.php' in link.get('href')):
-                
                 response = s.get(link.get('href'))
+
+                # Extract the file name and put the file in an appropriate directory
                 filename = urllib.parse.unquote(response.url.split("/")[-1])
                 filepath = os.path.join(selected_course_name, filename)
                 
@@ -71,4 +78,4 @@ with Session() as s:
                 with open(filepath, "wb") as download:
                     download.write(response.content)
 
-                print("File " + filepath + " downloaded")
+                print("Downloaded " + filepath)
