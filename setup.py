4a22b2ad556decc161d41c74be73de49b582c17d
Satvik Saha <sahasatvik@gmail.com>
2021-04-10 10:41:54 +0530

    Configured pypi distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "welearn-bot-iiserkol",
+    description = "A command line client for WeLearn, in the IISER Kolkata domain",
+    author = "Parth Bibekar",
+    author_email = "bibekarparth24@gmail.com",
+    url = "https://github.com/ParthBibekar/Welearn-bot",
+    version = "1.0.0",
+    license = "MIT",
+    scripts = ["welearn_bot"]
+)
