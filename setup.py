530872363372c01fa90c7b1b0fbfab2a5c0fce5f
Satvik Saha <sahasatvik@gmail.com>
2021-04-10 10:56:54 +0530

    Added long_description to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
 requirements = []
-with open('requirements.txt', 'r') as fh:
+with open("requirements.txt", "r") as fh:
     for line in fh:
         requirements.append(line.strip())
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name = "welearn-bot-iiserkol",
     description = "A command line client for WeLearn, in the IISER Kolkata domain",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author = "Parth Bibekar",
     author_email = "bibekarparth24@gmail.com",
     url = "https://github.com/ParthBibekar/Welearn-bot",
