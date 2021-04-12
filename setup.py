4f03b3589b7ad7192f22a16582a2d171b78c76d1
Satvik Saha <sahasatvik@gmail.com>
2021-04-12 14:52:32 +0530

    Release v1.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author = "Parth Bibekar",
     author_email = "bibekarparth24@gmail.com",
     url = "https://github.com/ParthBibekar/Welearn-bot",
-    version = "1.0.1",
+    version = "1.2.0",
     license = "MIT",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
