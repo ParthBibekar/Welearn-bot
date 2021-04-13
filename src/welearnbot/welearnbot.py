3139195a1611f24cb31840835348778a3c72ade6
Satvik Saha <sahasatvik@gmail.com>
2021-04-13 15:35:02 +0530

    Added --version flag

--- src/welearnbot/welearnbot.py
+++ src/welearnbot/welearnbot.py
@@ -72,6 +72,7 @@ def main():
         whoami      - shows the user's name and exits\n\
     Abbreviations such as any one of 'f', 'a', 'u', 'c', 'w' are supported.")
     parser.add_argument("courses", nargs="*", help="IDs of the courses to download files from. The word ALL selects everything from the [courses] section in .welearnrc or welearn.ini")
+    parser.add_argument("-v", "--version", action="version",  version="1.2.2a1")
     parser.add_argument("-d", "--dueassignments", action="store_true", help="show only due assignments with the 'assignments' action")
     parser.add_argument("-c", "--gcalendar", action="store_true", help="add due assignments to Google Calendar with the 'assignments' action")
     parser.add_argument("-i", "--ignoretypes", nargs="*", help="ignores the specified extensions when downloading, overrides .welearnrc")
