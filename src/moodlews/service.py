8c3a49da05b2ec617bfa59da495adf510bf34140
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-01-30 21:12:31 +0530

    autofill token while using MoodleClient.response

--- src/moodlews/service.py
+++ src/moodlews/service.py
@@ -2,6 +2,7 @@ from requests import Session
 import urllib.parse
 import json
 
+
 class ServerFunctions:
     SITE_INFO = "core_webservice_get_site_info"
     ALL_COURSES = "core_course_get_courses_by_field"
@@ -12,33 +13,36 @@ class ServerFunctions:
     URLS = "mod_url_get_urls_by_courses"
     RESOURCES = "mod_resource_get_resources_by_courses"
 
+
 class MoodleClient:
     def __init__(self, baseurl):
         self.baseurl = baseurl
         self.login_url = urllib.parse.urljoin(baseurl, "login/token.php")
         self.server_url = urllib.parse.urljoin(baseurl, "webservice/rest/server.php")
         self.session = Session()
+        self.token = ""
 
     def response(self, url, **data):
-        return self.session.post(url, data)
-    
+        tokenized_data = {"token": self.token, **data}
+        return self.session.post(url, tokenized_data)
+
     def response_json(self, url, **data):
         response = self.response(url, **data)
         return json.loads(response.content)
-    
+
     def authenticate(self, username, password):
         login = self.response_json(
             self.login_url,
             username=username,
             password=password,
-            service="moodle_mobile_app"
+            service="moodle_mobile_app",
         )
         try:
-            self.token = login['token']
+            self.token = login["token"]
             return self.token
         except KeyError:
             return False
-    
+
     def server(self, function, **data):
         return self.response_json(
             self.server_url,
